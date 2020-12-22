5a5d35a74384a3fc0e1c889c77e02bc92f62578f
Rafia <rafiazaheer155@yahoo.com>
2020-12-22 11:22:35 +0500

    Initial Commit

---
+++ CheckGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckGit
+//
+//  Created by Digital Landscape  on 22/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
